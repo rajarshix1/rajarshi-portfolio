c3620f014b96bd0dbc9e17867382f39c1b0c691c
Rajarshi Mandal <rajarshim5@gmail.com>
2025-04-05 14:06:03 +0530

    responsive fix and button colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
           WebSocket, and payment gateway integrations, with expertise in building high-performance, data-driven
           applications.
         </p>
-        <div className="flex flex-col md:flex-row space-x-0 md:space-x-4 ">
-        <button className="px-3 py-2 rounded-3xl bg-primary hover:bg-red-700 hover:cursor-pointer">Contact Me</button>
-        <a  href="/Resume-Rajarshi-BE.pdf" download><button className="px-3 py-2 rounded-3xl bg-primary hover:bg-red-700 hover:cursor-pointer">View Resume</button></a>
+        <div className="flex flex-row justify-center space-x-4 ">
+        <button className="px-3 py-2 w-fit rounded-3xl bg-primary hover:bg-red-700 hover:cursor-pointer">Contact Me</button>
+        <a className="px-3 py-2 w-fit rounded-3xl bg-blue-700 hover:bg-red-700 hover:cursor-pointer flex items-center justify-center" href="/Resume-Rajarshi-BE.pdf" download><button >View Resume</button></a>
         </div>
       </div>
       {/* <div className="w-full md:w-5/12  bg-gradient-to-tl from-fuchsia-100 to-pink-400 p-1 rounded-4xl mt-4 md:mr-2"> */}
