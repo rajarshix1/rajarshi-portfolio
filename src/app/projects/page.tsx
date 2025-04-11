aa81c412aec662ccba2b211effab398eaafd4e72
Rajarshi Mandal <rajarshim5@gmail.com>
2025-04-11 14:58:27 +0530

    projects page updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,11 +10,13 @@ export default function Projects() {
     <div className={`flex flex-col h-full font-mono ${darkTheme? `bg-secondary text-accent` : `bg-white text-blue-950`}`}>
         <Navbar/>
         <div className='w-full my-2 md:my-6 px-1 md:px-6 flex flex-col md:flex-row text-center justify-center space-y-2'>
-           <div className='p-2 md:p-20 md:w-1/2'>
+           <div className='p-2 md:p-6 md:w-1/2'>
            <p>With 3+ years of experience, I have done multiple projects both in frontend and in backend.<br/> I have used JS, TS, Node.js, React, MongoDB, SQL, Welsockets and more technologies extensively.</p> 
            <p>My projects includes : Online Marketplaces, Chat applications, Management systems, Payment systems, Real time location tracking, Crypto exchange, Block explorers and more.</p>
            </div>
-            <Image className='px-2 md:w-1/2 rounded-4xl' src="/gp.webp" alt="Github profile" height={300} width={600} />
+           <div  className='px-2 md:w-1/2  flex justify-center items-center p-5 ' >
+            <Image className='rounded-[10px] shadow-xl shadow-emerald-600' src="/gp.webp" alt="Github profile" height={300} width={600} />
+           </div>
         </div>
         {/* <div className=''>Project2</div>
         <div className=''>Project3</div>
